8843cec9beb1eaf53c30c07cb13c68fce6d9b899
mukundshelake <71928780+mukundshelake@users.noreply.github.com>
2025-11-24 09:21:52 +0000

    Deploying to gh-pages from @ mkshp-dev/obsidian-finance-plugin@f5ebc5ef71018b925e80077c8934aca94f8de2b2 🚀

---
+++ assets/js/bbffbbc6.595c2cbd.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkdocs_site=globalThis.webpackChunkdocs_site||[]).push([[1452],{4242:a=>{a.exports=JSON.parse('{"metadata":{"permalink":"/obsidian-finance-plugin/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
