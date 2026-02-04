c86a5e31398222bed3345f1b18338121da48bcfc
Mukund Shelake <mukundshelake400@gmail.com>
2026-02-04 22:17:14 +0530

    feat: Add primary logo to homepage header

--- docs-site/src/pages/index.tsx
+++ docs-site/src/pages/index.tsx
@@ -13,6 +13,11 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
+        <img 
+          src="img/Primary_horizontal_logo.png" 
+          alt="Beancount for Obsidian Logo" 
+          style={{maxWidth: '400px', marginBottom: '2rem'}}
+        />
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
